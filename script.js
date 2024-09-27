206d790c05d347fc5ba424a6ae6074fa6a5e2afa
chouhan-ayushi <chouhanayushi225@gmail.com>
2024-09-27 13:13:42 +0530

    git flow

---
+++ script.js
@@ -0,0 +1,126 @@
+//let age=17;
+//if(age>=18){
+   // console.log("you are eligible");
+//}
+//else{
+//console.log("you are not eligible")
+//}
+
+
+
+// let signal="pink";
+// if (signal=="red"){
+//    console.log("red=stop");
+// }
+// else if (signal=="yellow"){
+//     console.log("yellow=go slow");
+// }
+// else if (signal=="green"){
+//     console.log("green=go");
+// }
+// else{
+//     console.log("invalid");
+// }
+
+
+
+//let user="hii";
+//switch(user){
+  //case "admin": 
+  //console.log("he is admin");
+  //break;
+  //case "student": 
+  //console.log("he is student");
+  //break;
+  //case "parent": 
+//   console.log("he is parent");
+//   break;
+//   default:
+//     console.log("invalid")
+//}
+
+
+// for(i=0; i<6; i++){
+//     console.log(i)
+// }
+
+// let i=0;
+// while(i<5){
+//     console.log(i);
+//     i++;
+// }
+
+
+// let i=0;
+// do{
+//     console.log("hii");
+//     i++
+// }
+// while(i<5){
+//     console.log(i);
+    
+// }
+
+// Ternary operator/ Ternary condition
+// condition ?true :false
+// let isloggedin= true;
+// isloggedin ?console.log("buy me a car") : console.log("not buying");
+
+//arrays
+//create an array
+// let a=[2,3,4,"sanket",false];
+// console.log(a);
+
+// let b= new Array();
+// console.log(b);
+
+// access and change any array
+// let a=[2,3,4,"sanket",false];
+//  console.log(a);
+// console.log(a[2]);
+// a[3]="Ayushi";
+// console.log(a);
+// console.log(a[6]);
+
+// methods in array
+// let arr=[2,4,5,6,"ayushi",3,true]
+// arr.push(89,9);
+// console.log(arr);
+ 
+// arr.pop();
+// console.log(arr);
+// let f=arr.shift();
+// console.log(f)
+// console.log(arr);
+// arr.unshift(0);
+// console.log(arr);
+// //convert array into strings
+// let a=[2,3,4];
+// let s=[7,8,9];
+// let d=a.concat(s);
+// console.log(a,s,d)
+// let f=d.join("")
+// console.log(f);
+// let g=d.join("@")
+// console.log(g);
+// //reverse array
+// d.reverse();
+// console.log(d);
+// // find index
+
+// console.log (d.indexOf(4));
+
+// let arr1=[3,4,5,6,7,8,9];
+// console.log(arr1.slice(2,3));
+
+// // arr1.splice(2,2,9,10);
+// console.log(arr1)
+
+
+// api = applcation programming interface ,mediator, frontend se backend tak request ,frontend ko backend tak connect krna
+
+var arr=[1,2,3];  
+
+var rev=arr.reverse();  
+
+console.log(rev);
